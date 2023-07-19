59c2ece6d3677ed91db240dea5b667384956a2e7
Henri <98414850+ProHen9@users.noreply.github.com>
2023-07-19 18:22:53 +0200

    eine weitere Section erstellt

---
+++ website/src/components/Home/SecondSection.tsx
@@ -0,0 +1,14 @@
+import { component$ } from "@builder.io/qwik";
+
+export default component$(() => {
+    return (
+        <div class="bg-coolgray-900">
+            <h2>
+                lerne programmieren und designen
+            </h2>
+            <p>
+                Lerne das Programmieren mit einer Liste und Bewertungen von Kursen. Wir stellen Kurse und andere wichtige Ressouren vor und erstellen aber auch selber welche.
+            </p>
+        </div>
+    )
+})
--- website/src/routes/index.tsx
+++ website/src/routes/index.tsx
@@ -1,10 +1,12 @@
 
 import { component$ } from "@builder.io/qwik";
 import FirstSection from "~/components/Home/FirstSection";
+import SecondSection from "~/components/Home/SecondSection";
 export default component$(() => {
   return (
     <>
       <FirstSection />
+      <SecondSection />
     </>
   );
 })
