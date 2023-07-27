ad7c2eee7e1b2098f16a8fa0cc506f0d90ae72a7
Henri <hengen.09@gmail.com>
2023-07-27 20:50:41 +0200

    Design weiter geändert

---
+++ zuhoeren/website/src/components/WorkLink.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    export let name: string;
+</script>
+
+<a href="/about/employees/{name}">@{name}</a>
+<style>
+    a {
+        text-decoration: none;
+        font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    }
+</style>
--- zuhoeren/website/src/routes/about/+page.svelte
+++ zuhoeren/website/src/routes/about/+page.svelte
@@ -1,9 +1,37 @@
+
+<script lang="ts">
+	import WorkLink from './../../components/WorkLink.svelte';
+    import { onMount } from "svelte";
+	let language = "en"
+
+	const date1: Date = new Date('2023-07-27');
+	const date2: Date = new Date();
+
+	const diffInMilliseconds: number = date2.getTime() - date1.getTime();
+
+
+	const diffInDays = Math.floor(diffInMilliseconds / (1000 * 60 * 60 * 24));
+
+	onMount(() => {
+		if (navigator.language == "de") {
+			language = "de"
+		}
+	})
+</script>
 <svelte:head>
-	<title>About</title>
-	<meta name="description" content="About this app" />
+	<title>zuhören | About us</title>
+	<meta name="description" content={language? "Zuhören ist eine Social-Media-App mit dem Focus auf lagem Kontent. Dabei werden Videos, Audios und Blogartikelt unterstützt.": "en"} />
 </svelte:head>
 
 <section>
-	<h3>Hey,</h3><br />
-	<p></p>
+	{#if language == "de"}
+		<h2>
+			Hey,
+		</h2>
+		<p>
+			dies ist eine relativ neue Social-Media-App, die erste Zeile Code wurde vor {diffInDays} Tagen geschrieben und das von dem Gründer <WorkLink name="Henri"/>. Unsere Mission war es, eine App zu entwickeln, die mal zur abwechslung Content bereitstellt, der etwas länger ist. Wir bieten die Möglichkeit Creatorn vor dem Content, kleine Werbespots zu posten. Als Consumer kann man sich Posts abspeichern, liken, disliken, kommentieren, Sharen, einer Liste hinzufügen, 
+		</p>
+	{:else}
+		<p>nö</p>
+	{/if}
 </section>
